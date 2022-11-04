d76b0fcb9aec80d86adcd29649971700a578865a
KathySega <ari.sega@gmail.com>
2022-11-04 00:54:19 -0600

    agregando registro usarios base

---
+++ FamilyMedicine-API/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using DTO;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FamilyMedicine_API.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        public string RegistrarUsuario(Usuario usuario)
+        {
+            return "TBD";
+        }
+
+        public string ActualizarUsuario(Usuario usuario)
+        {
+            return "TBD";
+        }
+
+        public string BorrarUsuario(Usuario usuario)
+        {
+            return "TBD";
+        }
+
+        public string ObtenerListaUsusarios()
+        {
+            return "TBD";
+        }
+
+        public string ObtenerUnUsuarios(int UsuarioId)
+        {
+            return "TBD";
+        }
+
+    }
+}
