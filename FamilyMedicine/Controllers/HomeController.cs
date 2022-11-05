4184a12bd2f56e5ef863e580e9027be0f2606619
Josué <psjosue1@gmail.com>
2022-11-04 19:57:05 -0600

    Todo el landing page

--- FamilyMedicine/Controllers/HomeController.cs
+++ FamilyMedicine/Controllers/HomeController.cs
@@ -36,5 +36,11 @@ namespace FamilyMedicine.Controllers
         {
             return View();
         }
+
+        public ActionResult LandingPage()
+        {
+            return View();
+        }
+        
     }
 }
