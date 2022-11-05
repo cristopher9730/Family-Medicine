783e67d0bfe85fc0bd67d086528c45973801215e
KathySega <ari.sega@gmail.com>
2022-11-05 10:48:40 -0600

    agregando el otp para registro usuario

---
+++ AppLogic/AdminCorreo.cs
@@ -0,0 +1,36 @@
+﻿using DTO;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppLogic
+{
+    public class AdminCorreo
+    {
+        
+
+        public void EviarEmailBienvenida(Usuario usuario)
+        {
+            StringBuilder emailBody = new StringBuilder();
+
+            var emailSubject = string.Format("Hemos");
+
+            emailBody.AppendLine(string.Format("Estimado {0}, Bienvenido a FamilyMedicine!", usuario.Nombre));
+            emailBody.AppendLine(string.Format("<br/>Tu codigo de seguridad:"));
+            emailBody.AppendLine(string.Format("<br/> Es un placer servirle!"));
+
+            EnviarEmail(usuario.Correo, emailSubject, emailBody.ToString());
+        }
+
+        private string EnviarEmail(string toEmail, string emailSubject, string emailBody)
+        {
+
+                return "TBD";
+        }
+
+    }
+}
---
+++ AppLogic/AdminSMS.cs
@@ -0,0 +1,32 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace AppLogic
+{
+    public class AdminSMS
+    {
+        public void EnviarSMS(Usuario usuario)
+        {
+
+            // Find your Account SID and Auth Token at twilio.com/console
+            // and set the environment variables. See http://twil.io/secure
+            string accountSid = "AC4d8f6c80ee0e98023f83c79b1eb01349";
+            string authToken = "574535f6f7faff66758fab15a401c41d";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "FamilyMedicine, Codigo de Seguridad: ",
+                from: new Twilio.Types.PhoneNumber("+16467984710"),
+                to: new Twilio.Types.PhoneNumber(usuario.Telefono)
+            );
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
