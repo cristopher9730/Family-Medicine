631c2e3494e23e2e80ff5e4065182d0afee8f859
Josué <psjosue1@gmail.com>
2022-11-07 18:00:05 -0600

    Recuperar contraseña

--- FamilyMedicine/Controllers/HomeController.cs
+++ FamilyMedicine/Controllers/HomeController.cs
@@ -41,6 +41,11 @@ namespace FamilyMedicine.Controllers
         {
             return View();
         }
+
+        public ActionResult forgotPassword()
+        {
+            return View();
+        }
         
     }
 }
