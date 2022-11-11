4101eea17e6dbc448b6709b27ab8f90f40f82e57
Francisco Rodriguez <frodriguezc@ucenfotec.ac.cr>
2022-11-10 19:06:17 -0600

    dashboard admin

---
+++ FamilyMedicine/Controllers/DashboardAdminLaboratorioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyMedicine.Controllers
+{
+    public class DashboardAdminLaboratorioController : Controller
+    {
+        public ActionResult DashboardAdminLaboratorio()
+        {
+            return View();
+        }
+    }
+}
