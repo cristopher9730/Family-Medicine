a1822596dbbe5ee51b502e2270830bfe0c67912b
Josué <psjosue1@gmail.com>
2022-11-12 13:13:27 -0600

    Cambios finales del dashboard con layout

---
+++ FamilyMedicine/Controllers/DashboardUsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyMedicine.Controllers
+{
+    public class DashboardUsuarioController : Controller
+    {
+        // GET: DashBoard
+        public ActionResult DashboardUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult DashBoardCitas()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Prueba()
+        {
+            JsonResult result = Json(new { result = "OK", data = "Prueba recibida" });
+            return result;
+        }
+    }
+}
