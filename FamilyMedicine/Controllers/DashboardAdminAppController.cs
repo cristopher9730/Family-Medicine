552db59eba2e4f28e5f2889d5e1ae4dbc3cb56f1
Cristopher Salazar <csalazara@ucenfotec.ac.cr>
2022-11-12 16:41:19 -0600

    Creacion de la pestaña de laboratorios

---
+++ FamilyMedicine/Controllers/DashboardAdminAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyMedicine.Controllers
+{
+    public class DashboardAdminAppController : Controller
+    {
+        // GET: DashboardAdminApp
+        public ActionResult LaboratoriosRegistrados()
+        {
+            return View();
+        }
+    }
+}
