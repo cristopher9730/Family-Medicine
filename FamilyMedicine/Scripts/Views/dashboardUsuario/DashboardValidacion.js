c8e8fce22236de4bc5572ddb76eb1d85eb91ef70
Josué <psjosue1@gmail.com>
2022-11-16 23:36:06 -0600

    Cambios semi finales

--- FamilyMedicine/Scripts/Views/dashboardUsuario/DashboardValidacion.js
+++ FamilyMedicine/Scripts/Views/dashboardUsuario/DashboardValidacion.js
@@ -26,3 +26,33 @@
         }
     })
 }
+
+
+function myfuncionGeneralCrear(ReporteCreado) {
+    Swal.fire({
+        title: ReporteCreado,
+        showDenyButton: true,
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        confirmButtonText: 'Guardar',
+        denyButtonText: `No guardar`,
+        confirmButtonColor: "#325D88",
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Creado!',
+                icon: 'success',
+                confirmButtonColor: "#325D88",
+                confirmButtonText: 'Hecho',
+            });
+        } else if (result.isDenied) {
+            Swal.fire({
+                title: 'No se creo',
+                icon: 'info',
+                confirmButtonColor: "#325D88",
+                confirmButtonText: 'Hecho',
+            });
+        }
+    })
+}
