3ea47278c3cc56d92861177e71e8987846f947f0
Josué <psjosue1@gmail.com>
2022-11-16 23:02:41 -0600

    Cambios con validaciones

--- FamilyMedicine/Scripts/Views/dashboardUsuario/DashboardCitasFechas.js
+++ FamilyMedicine/Scripts/Views/dashboardUsuario/DashboardCitasFechas.js
@@ -1,7 +1,8 @@
 ﻿const str = " Fecha: 2022/11/27 a las 16:00\n" +
     " Costo pagado de cita: ₡12000\n";
 
-function myFunctionDate() {
+/*
+function myFunctionDateAgendada() {
     Swal.fire({
         title: 'Fecha de cita',
         html: '<pre>' + str + '</pre>',
@@ -9,6 +10,76 @@ function myFunctionDate() {
         confirmButtonText: 'Hecho',
         customClass: {
             popup: 'format-pre'
+        }
+    });
+}*/
+
+
+function myFunctionDateAgendada() {
+    Swal.fire({
+        title: 'Quieres Agendar fecha de cita"',
+        showDenyButton: true,
+        showCancelButton: true,
+        cancelButtonText: "Cerrar",
+        confirmButtonText: 'Ver cita',
+        denyButtonText: `Cancelar cita`,
+        confirmButtonColor: "#325D88",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Fecha de cita',
+                html: '<pre>' + str + '</pre>',
+                confirmButtonColor: "#325D88",
+                confirmButtonText: 'Hecho',
+                customClass: {
+                    popup: 'format-pre'
+                }
+            });
+        } else if (result.isDenied) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Quieres cancelar cita?',
+                showDenyButton: true,
+                showCancelButton: true,
+                cancelButtonText: "Cerrar ya no quiero",
+                confirmButtonText: 'Si cancelar',
+                confirmButtonColor: "#325D88",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        icon: 'info',
+                        title: 'Si se cancelo la cita',
+                        confirmButtonText: 'Hecho',
+                        confirmButtonColor: "#325D88",
+                    });
+                } else if (result.isDenied) {
+                    Swal.fire({
+                        icon: 'info',
+                        title: 'No se cancelo la cita',
+                        confirmButtonText: 'Hecho',
+                        confirmButtonColor: "#325D88",
+                    });
+                }
+            });
+        }
+    });
+}
+
+
+
+
+function myFunctionDate() {
+    Swal.fire({
+        title: "Agendar fecha de cita",
+        html: '<input id="datetimepicker"  type="datetime-local" class="form-control" autofocus>',
+        type: "warning",
+        confirmButtonColor: "#325D88",
+        confirmButtonText: 'Hecho',
+        onOpen: function () {
+            $('#datetimepicker').datetimepicker({
+                format: 'DD/MM/YYYY hh:mm A',
+                defaultDate: new Date()
+            });
         }
     });
 }
---
+++ FamilyMedicine/Scripts/Views/dashboardUsuario/DashboardValidacion.js
@@ -0,0 +1,28 @@
+﻿function myfuncionGeneral() {
+    Swal.fire({
+        title: 'Quieres guardar los cambios',
+        showDenyButton: true,
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        confirmButtonText: 'Guardar',
+        denyButtonText: `No guardar`,
+        confirmButtonColor: "#325D88",
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Guardado!',
+                icon: 'success',
+                confirmButtonColor: "#325D88",
+                confirmButtonText: 'Hecho',
+            });
+        } else if (result.isDenied) {
+            Swal.fire({
+                title: 'Los cambios no se guardaron',
+                icon: 'info',
+                confirmButtonColor: "#325D88",
+                confirmButtonText: 'Hecho',
+            });  
+        }
+    })
+}
