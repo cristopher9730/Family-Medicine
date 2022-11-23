ef36d377194ea08a45287d99aef897415023fb0d
Josué <psjosue1@gmail.com>
2022-11-22 21:58:53 -0600

    Distribucion de carpetas

---
+++ FamilyMedicine/Controllers/IniciarSecionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyMedicine.Controllers
+{
+    public class IniciarSecionController : Controller
+    {
+        // GET: IniciarSecion
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult OTP()
+        {
+            return View();
+        }
+    }
+}
