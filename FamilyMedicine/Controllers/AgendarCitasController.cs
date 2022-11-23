7291ab979384ecff0fcc0511f05072b01dbbd21e
Josué <psjosue1@gmail.com>
2022-11-22 20:02:53 -0600

    Cambios de feedback

---
+++ FamilyMedicine/Controllers/AgendarCitasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyMedicine.Controllers
+{
+    public class AgendarCitasController : Controller
+    {
+        // GET: AgendarCitas
+        public ActionResult AgendarCitas()
+        {
+            return View();
+        }
+    }
+}
