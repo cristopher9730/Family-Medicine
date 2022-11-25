0b653a1ba17f7be570fe5e501d5234507e917036
KathySega <ari.sega@gmail.com>
2022-11-24 22:26:23 -0600

    +otp

---
+++ DTO/OTP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class OTP:EntidadBase
+    {
+        public int usuarioId { get; set; }
+        public int usuarioCodigo { get; set; }
+
+        public DateTime fechaHoraCreacion { get; set; } 
+
+        public string estado { get; set; }
+    }
+}
---
+++ FamilyMedicine/Scripts/Views/dashboardUsuario/OTP.js
@@ -0,0 +1,32 @@
+﻿function OTP() {
+    this.InitView = function () {
+        $("#btnRegistrarse").click(function () {
+            var vista = new OTP();
+            vista.ValidarOTP();
+        });
+    }
+
+    this.ValidarOTP = function () {
+        var otp = {}
+        usuario.Nombre = $("#txtNombre").val();
+       
+
+        $.ajax({
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            method: "POST",
+            url: "https://localhost:44391/api/Usuario/RegistrarUsuario",
+            contentType: "application/json",
+            data: JSON.stringify(usuario),
+            hasContent: true
+        }).done(function (info) {
+            alert('Usuario Creado correctamente');
+            window.location.href = "/IniciarSesion/OTP";
+        }
+        ).fail(function (info) {
+            alert('hubo un problema al crear usuario');
+        });
+    }
+}
