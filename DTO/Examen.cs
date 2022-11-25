43469556f3956987b6e981136d315a9711d67557
Josué <psjosue1@gmail.com>
2022-11-24 22:08:57 -0600

    Validacion de formularios y mejora de registro

---
+++ DTO/Examen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DTO
+{
+    public class Examen
+    {
+        public int Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Descripccion { get; set; }
+        public string Laboratorio { get; set; }
+        public float Precio { get; set; }
+    }
+}
