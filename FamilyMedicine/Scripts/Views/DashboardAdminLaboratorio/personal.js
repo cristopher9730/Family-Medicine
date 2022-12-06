ceb8f39c0305f5db562c556742569eb88fdb5de9
KathySega <ari.sega@gmail.com>
2022-12-05 22:00:47 -0600

    agragando personal

---
+++ FamilyMedicine/Scripts/Views/DashboardAdminLaboratorio/personal.js
@@ -0,0 +1,51 @@
+﻿function Personal() {
+
+    this.InitView = function () {
+        $("#btnRegistrarPersonal").click(function () {
+            var vista = new Personal();
+            vista.registrarPersonal();
+        });
+    }
+
+    this.cargarLaboratorio = function () {
+
+    }
+
+    this.registrarPersonal = function () {
+        var usuario = {}
+        usuario.Nombre = $("#txtNombre").val();
+        usuario.PrimerApellido = $("#txtPrimerApellido").val();
+        usuario.SegundoApellido = $("#txtSegundoApellido").val();
+        usuario.Correo = $("#txtCorreo").val();
+        usuario.Telefono = $("#txtTelefono").val();
+        usuario.Clave = $("#txtClave").val();
+        usuario.Foto = "Foto";
+        usuario.Estado = "Pendiente";
+        usuario.RolId = $("#slt_rol").val();
+        usuario.MembresiaId = 0;
+        usuario.LaboratorioId = 0;
+
+        $.ajax({
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            method: "POST",
+            url: "https://familymedicine-api.azurewebsites.net/api/Usuario/RegistrarUsuario",
+            contentType: "application/json",
+            data: JSON.stringify(usuario),
+            hasContent: true
+        }).done(function (info) {
+            alert('Usuario registrado correctamente');
+        }
+        ).fail(function (info) {
+            alert('hubo un problema al registrar usuario');
+        });
+    }
+}
+
+$(document).ready(function () {
+    $.noConflict();
+    var view = new Personal();
+    view.InitView();
+});
